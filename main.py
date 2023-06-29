d3144a5dc4eacc002c8efe2973ec8b1bd1aded8f
Ankit Das <96786190+PiKa919@users.noreply.github.com>
2023-06-30 03:43:35 +0530

    Plotted make vs range

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('Electric_Vehicle_Population_Data copy.csv')
 
-#Check the structure and contents of the dataset using 
-print(df.head())
-df.info()
+# #Check the structure and contents of the dataset using 
+# print(df.head())
+# df.info()
 
-#Examine summary statistics using
-df.describe()
+# #Examine summary statistics using
+# df.describe()
 
-#Check for missing values with
-df.isnull().sum()
+# #Check for missing values with
+# df.isnull().sum()
+
+df = df.drop('Legislative District', axis=1)
+print(df.info())
+
