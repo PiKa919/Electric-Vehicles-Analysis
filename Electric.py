debed8799d19fbf6ea019d07d3312a41d80d6632
Ankit Das <96786190+PiKa919@users.noreply.github.com>
2023-06-30 03:02:33 +0530

    started jupyter and basic tests

--- Electric.py
+++ Electric.py
@@ -1,15 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+# In[3]:
+
+
 df = pd.read_csv('Electric_Vehicle_Population_Data copy.csv')
+
+
+# In[4]:
+
 
 #Check the structure and contents of the dataset using 
 print(df.head())
 df.info()
 
+
+# In[6]:
+
+
 #Examine summary statistics using
 df.describe()
 
+
+# In[8]:
+
+
 #Check for missing values with
 df.isnull().sum()
+
+
+# In[ ]:
+
+
+
+
